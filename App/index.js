7de3f006968d4546ff6bd44d61d78c47d47e424a
developeroliver <developpeur.olive@gmail.com>
2023-02-26 15:31:23 +0100

    Design HomeScreen add logo and text create button with state

--- App/index.js
+++ App/index.js
@@ -1,14 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  Image,
+  Pressable,
+} from 'react-native';
+import { scale } from 'react-native-size-matters';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello React Native!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import starbucks from '../assets/starbucks.png';
 
 const styles = StyleSheet.create({
   container: {
@@ -17,4 +19,41 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  image: {
+    width: scale(100),
+    height: scale(100),
+    marginBottom: scale(15),
+  },
+  title: {
+    fontSize: scale(32),
+    fontWeight: 'bold',
+    color: '#00704A',
+    textTransform: 'uppercase',
+  },
 });
+
+export default function App() {
+  const [ordered, setOrdered] = useState(false);
+
+  return (
+    <View style={styles.container}>
+      <Image
+        source={starbucks}
+        style={styles.image}
+        resizeMode="contain"
+      />
+      <StatusBar style="auto" />
+      <Text style={styles.title}>Starbucks!</Text>
+      <Pressable
+        onPress={() => setOrdered(!ordered)}
+        style={styles.button}
+      >
+        {ordered ? (
+          <Text>Commande en cours</Text>
+        ) : (
+          <Text>Passer commande</Text>
+        )}
+      </Pressable>
+    </View>
+  );
+}
